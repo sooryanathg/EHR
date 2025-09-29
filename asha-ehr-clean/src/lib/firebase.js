212ba97410b06814f601ac4ad49bfa88b1dc0f8a
sooryanathg <sooryanathgopi@gmail.com>
2025-09-29 15:09:27 +0530

    env example

--- asha-ehr-clean/src/lib/firebase.js
+++ asha-ehr-clean/src/lib/firebase.js
@@ -5,12 +5,12 @@ import { getStorage } from 'firebase/storage';
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBHFZB9455Y8KmucLkNpzd2X2sEpjg7r3k",
-  authDomain: "nexus-it-44cb1.firebaseapp.com",
-  projectId: "nexus-it-44cb1",
-  storageBucket: "nexus-it-44cb1.appspot.com",
-  messagingSenderId: "505288933029",
-  appId: "1:505288933029:web:b18b502726f9eb1d9e974c"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
