ea7a762f36ab913fb5e8f912c12158a67132dfc6
sooryanathg <sooryanathgopi@gmail.com>
2025-10-04 11:43:39 +0530

    minor changes 4

--- asha-ehr-clean/android/app/src/main/java/com/nexus/ashaehr/MainApplication.kt
+++ asha-ehr-clean/android/app/src/main/java/com/nexus/ashaehr/MainApplication.kt
@@ -15,7 +15,6 @@ import com.facebook.react.defaults.DefaultReactNativeHost
 
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
-import com.wenkesj.voice.VoicePackage
 
 class MainApplication : Application(), ReactApplication {
 
@@ -25,7 +24,7 @@ class MainApplication : Application(), ReactApplication {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
-              add(VoicePackage())
+              // add(MyReactNativePackage())
             }
 
           override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
